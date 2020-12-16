7f80afa183ee974068ba06a6cdf412aadef0491b
Ryan Beckett <rybecket@microsoft.com>
2020-12-16 09:43:08 -0700

    rename pfc model tests

---
+++ ZenLibTests/ModelTests.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ModelTests.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace ZenLib.Tests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Tests for primitive types.
+    /// </summary>
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ModelTests
+    {
+        /// <summary>
+        /// Test input generation for the PFC model.
+        /// </summary>
+        [TestMethod]
+        public void TestInputGenerationPfc()
+        {
+            Assert.AreEqual(8, PfcModel.GenerateTests().Count());
+        }
+    }
+}
