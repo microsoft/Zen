420ccf32ec0cf2e02c521d0b7b7ed3c43f55107c
Ryan Beckett <rybecket@microsoft.com>
2022-02-07 23:04:18 -0800

    add custom size attribute for Zen objects

---
+++ ZenLib/Common/ZenSizeAttribute.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="ZenSizeAttribute.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace ZenLib
+{
+    using System;
+
+    /// <summary>
+    /// Attribute annotation for fields that provides sizing information to Zen when
+    /// generating symbolic values of a given type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class ZenSizeAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ZenSizeAttribute"/> class.
+        /// </summary>
+        /// <param name="depth">The depth for this field.</param>
+        /// <param name="exhaustiveDepth">Whether to exit on the first failure.</param>
+        /// <param name="recursive">Whether to apply these setting recursively.</param>
+        public ZenSizeAttribute(int? depth = null, bool? exhaustiveDepth = null, bool recursive = false)
+        {
+            this.Depth = depth;
+            this.ExhaustiveDepth = exhaustiveDepth;
+            this.Recursive = recursive;
+        }
+
+        /// <summary>
+        /// Gets the size depth for the field or property.
+        /// </summary>
+        public int? Depth { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to generate exhaustive depth objects.
+        /// </summary>
+        public bool? ExhaustiveDepth { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to apply these settings recursively.
+        /// </summary>
+        public bool Recursive { get; }
+    }
+}
--- ZenLib/Generation/SymbolicInputGenerator.cs
+++ ZenLib/Generation/SymbolicInputGenerator.cs
@@ -127,10 +127,37 @@ namespace ZenLib.Generation
             var args = new (string, object)[asList.Length];
             for (int i = 0; i < asList.Length; i++)
             {
-                args[i] = (asList[i].Key, recurse(asList[i].Value, config));
+                var fieldName = asList[i].Key;
+                var fieldType = asList[i].Value;
+                config = UpdateDepthConfiguration(config, GetSizeAttribute(objectType, fieldName));
+                args[i] = (fieldName, recurse(fieldType, config));
             }
 
             return method.Invoke(null, new object[] { args });
+        }
+
+        private DepthConfiguration UpdateDepthConfiguration(DepthConfiguration config, ZenSizeAttribute attribute)
+        {
+            if (attribute == null)
+            {
+                return config;
+            }
+
+            var depth = attribute.Depth.HasValue ? attribute.Depth.Value : config.Depth;
+            var exhaustive = attribute.ExhaustiveDepth.HasValue ? attribute.ExhaustiveDepth.Value : config.ExhaustiveDepth;
+            return new DepthConfiguration { Depth = depth, ExhaustiveDepth = exhaustive };
+        }
+
+        private ZenSizeAttribute GetSizeAttribute(Type type, string fieldName)
+        {
+            var fieldInfo = type.GetField(fieldName);
+            if (fieldInfo != null)
+            {
+                return (ZenSizeAttribute)fieldInfo.GetCustomAttribute(typeof(ZenSizeAttribute));
+            }
+
+            var propertyInfo = type.GetPropertyCached(fieldName);
+            return (ZenSizeAttribute)propertyInfo.GetCustomAttribute(typeof(ZenSizeAttribute));
         }
 
         public object VisitShort()
