fa136e206709a71c02d08d7b9968c0a4dbf87b4e
Ryan Beckett <rybecket@microsoft.com>
2022-04-06 21:38:25 -0700

    add source code generator for field accessors in object types

---
+++ ZenLib.Generators.Tests/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ZenLib.Generators.Tests;
+
+using ZenLib;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        var x = Zen.Symbolic<Foo>();
+        x.GetId();
+        x.GetBar();
+        x.WithBar(Zen.Symbolic<int>());
+        x.WithId(Zen.Symbolic<Real>());
+
+        var y = Zen.Symbolic<Fat<int>>();
+        y.GetField();
+        y.WithField(0);
+
+        var z = Zen.Symbolic<Bar>();
+        z.GetBlah();
+        z.WithBlah(0);
+    }
+}
+
+[ZenObject]
+public class Foo
+{
+    public Real Id { get; set; }
+
+    public int Bar { get; set; }
+}
+
+[ZenObject]
+public class Bar
+{
+    public uint Blah;
+}
+
+[ZenObject]
+public struct Fat<T>
+{
+    public T Field { get; set; }
+
+    private int Other;
+}
---
+++ ZenLib/ZenObjectAttribute.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="ZenObjectAttribute.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace ZenLib
+{
+    using System;
+
+    /// <summary>
+    /// Attribute annotation for classes and structs to generate zen methods.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+    public class ZenObjectAttribute : Attribute
+    {
+    }
+}
